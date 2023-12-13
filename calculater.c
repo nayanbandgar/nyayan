3ec506d995e11f55c81d786ec1775dcfedce0b9e
nayanbandgar <148048997+nayanbandgar@users.noreply.github.com>
2023-12-13 13:52:36 +0530

    Add files via upload

---
+++ calculater.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("plese enter 2values for addittion:-");
+    scanf("%d %d", &a,&b);
+    printf("addition of 2 numbers is=%d", (a-b));
+
+    return 0;
+}
